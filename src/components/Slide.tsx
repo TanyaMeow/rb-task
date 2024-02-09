20d02d23430d6238994540b95720e1c0aafe12dc
Татьяна Хонякина <hanakinat@gmail.com>
2024-02-09 15:02:47 +0300

    Создан компонент слайда

--- src/components/Slide.tsx
+++ src/components/Slide.tsx
@@ -3,28 +3,31 @@ import doctor from "../image/doctor-working-table 1.jpg";
 
 export function Slide() {
     return (
-        <>
-            <div className='path'></div>
-            <div className='slider_title'>
-                <h1 className='title_slide'>Check-UP</h1>
-                <h2 className='subtitle_slide'>для мужчин</h2>
-                <div className='title_list'>
-                    <ul>
-                        <li>Гормональный скрининг</li>
-                        <li>Тестостерон</li>
-                        <li>Свободный тестостерон</li>
-                        <li>Глобулин, связывающий половые гормоны</li>
-                    </ul>
+        <div className='slider_content'>
+                <div className='path'></div>
+
+                <div className='slider_title'>
+                    <h1 className='title_slide'>Check-UP</h1>
+                    <h2 className='subtitle_slide'>для мужчин</h2>
+                    <div className='title_list'>
+                        <ul>
+                            <li>Гормональный скрининг</li>
+                            <li>Тестостерон</li>
+                            <li>Свободный тестостерон</li>
+                            <li>Глобулин, связывающий половые гормоны</li>
+                        </ul>
+                    </div>
+                    <div className='price'>
+                        <p className='new_price'>Всего 2800₽</p>
+                        <p className='old_price'><s>3500₽</s></p>
+                    </div>
+                    <div className='slider_button'>
+                        <Button text='Записаться' class='green slide_button mobile'/>
+                        <Button text='Подробнее' class='more mobile'/>
+                    </div>
                 </div>
-                <div className='price'>
-                    <p className='new_price'>Всего 2800₽</p>
-                    <p className='old_price'><s>3500₽</s></p>
-                </div>
-                <div className='slider_button'>
-                    <Button text='Записаться' class='green slide_button mobile' />
-                    <Button text='Подробнее' class='more mobile' />
-                </div>
+            <img className='picture_slider' src={doctor} alt=""/>
             </div>
-            <img src={doctor} alt=""/></>
+
     )
 }
