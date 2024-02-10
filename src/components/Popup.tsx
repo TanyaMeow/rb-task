522605c80d522082e6f60500ba84a5ff6dda1491
Татьяна Хонякина <hanakinat@gmail.com>
2024-02-10 13:30:49 +0300

    Реализация закрытия модального окна

--- src/components/Popup.tsx
+++ src/components/Popup.tsx
@@ -1,21 +1,24 @@
 import closeLight from '../image/button_close-light.svg';
 import {Button} from "./Button";
 import {useContext} from "react";
-import {PopupContext} from "../context/PopupContext";
+import {PopupContext, SetPopupOpenContext} from "../context/PopupContext";
 
 export function Popup(props: any) {
     const open: boolean = useContext(PopupContext);
+    const setClosePopup = useContext(SetPopupOpenContext);
 
     return (
         <div className='popup_block' style={{display: open ? 'flex' : 'none'}}>
             <div className='wrapper'>
                 <div className='popup_container'>
-                    <div className='popup_close'>
+                    <div onClick={() => setClosePopup(false)}
+                         className='popup_close'>
                         <img src={closeLight} alt=""/>
                     </div>
                     <div className='popup_body'>
                         <div className='popup_title'>
-                            <div className='close'></div>
+                            <div onClick={() => setClosePopup(false)}
+                                 className='close'></div>
                             <h1 className='title'>Запишитесь <br/> на приём онлайн</h1>
                             <p className='description'>Администратор свяжется с вами через WhatsApp в течение дня и уточнит детали</p>
                         </div>
