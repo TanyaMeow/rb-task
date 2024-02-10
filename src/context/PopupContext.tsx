a1bfb70562f82982dc241fbbaa82600c8eaf8668
Татьяна Хонякина <hanakinat@gmail.com>
2024-02-10 13:49:42 +0300

    Добавлен тип для контекста отвечающего за переключение модального окна

--- src/context/PopupContext.tsx
+++ src/context/PopupContext.tsx
@@ -3,4 +3,4 @@ import {createContext} from "react";
 type setOpen = (active: boolean) => void;
 
 export const PopupContext = createContext<boolean>(false);
-export const SetPopupOpenContext = createContext<setOpen>(() => {})
+export const SetPopupOpenContext = createContext<setOpen>(() => {});
